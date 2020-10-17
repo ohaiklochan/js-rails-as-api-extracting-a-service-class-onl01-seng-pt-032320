03b418ad5dc6634358714297c7f6d67b69aff3b6
Chloe Steffens <caforonda103@yahoo.com>
2020-10-17 03:29:55 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -6,9 +6,12 @@ class SightingsController < ApplicationController
       :location => {:only => [:latitude, :longitude]}
     }, :except => [:updated_at])
   end
-    
+ 
   def show
-    @sighting = Sighting.find(params[:id])
-    render json: @sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
+    sighting = Sighting.find_by(id: params[:id])
+    render json: sighting.to_json(:include => {
+      :bird => {:only => [:name, :species]},
+      :location => {:only => [:latitude, :longitude]}
+    }, :except => [:updated_at]
   end
 end
